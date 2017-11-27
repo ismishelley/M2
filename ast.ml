dc11dff424ed8c985b1b7e24e018f2863612ef41
ismishelley <33074955+ismishelley@users.noreply.github.com>
2017-11-26 20:20:54 -0500

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -1,111 +1,55 @@
-type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
-          And | Or
+(* Binary Operators *)
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq | And | Or
 
-type uop = Neg | Not
+(* Unary Operators *)
+type uop = Neg | Not | Inc | Dec
 
-type typ = Int | Float | String | Bool | Void | Matrix of typ * int * int
+(* Nums *)
+type num = IntLit of int | FloatLit of float
 
-type bind = typ * string
+(* Data Types *)
+type primitives = Int | Float | Bool | Void	| String | Matrix of primitives * num * num
+type datatype = Datatype of primitives
 
+type formal = Formal of datatype * string
+type local = Local of datatype * string
+type var_dec = datatype * string
+
+(* Expressions *)
 type expr =
-   IntLit of int
-  | FloatLit of float
-  | StringLit of string
-  | BoolLit of bool
-  | MatrixLit of expr list list
-  | Id of string
-  | Binop of expr * op * expr
-  | Unop of uop * expr
-  | Assign of string * expr
-  | Call of string * expr list
-  | Noexpr
+	| NumLit of num
+	| BoolLit of bool
+	| StringLit of string
+	| MatrixLit of num list list
+	| Id of string
+	| Noexpr
+	| Null
+	| Binop of expr * op * expr
+	| Unop of uop * expr
+	| Assign of expr * expr
+	| Call of string * expr list
+	| MatrixAccess of string * expr * expr
+	| Rows of string
+	| Cols of string
+	| Transpose of string
 
+(* Statements *)
 type stmt =
-    Block of stmt list
-  | Expr of expr
-  | Return of expr
-  | If of expr * stmt * stmt
-  | For of expr * expr * expr * stmt
-  | While of expr * stmt
+	| Block of stmt list
+	| Expr of expr
+	| If of expr * stmt * stmt
+	| For of expr * expr * expr * stmt
+	| While of expr * stmt
+	| Return of expr
 
+(* Function Declarations *)
 type func_decl = {
-    typ : typ;
-    fname : string;
-    formals : bind list;
-    locals : bind list;
-    body : stmt list;
-  }
+	return_type : datatype;
+	fname 	  : string;
+	formals 	  : formal list;
+	locals  	  : local list;
+	body        : stmt list;
+}
 
-type program = bind list * func_decl list
-
-(* Pretty-printing functions *)
-
-let string_of_op = function
-    Add -> "+"
-  | Sub -> "-"
-  | Mult -> "*"
-  | Div -> "/"
-  | Equal -> "=="
-  | Neq -> "!="
-  | Less -> "<"
-  | Leq -> "<="
-  | Greater -> ">"
-  | Geq -> ">="
-  | And -> "&&"
-  | Or -> "||"
-
-let string_of_uop = function
-    Neg -> "-"
-  | Not -> "!"
-
-let rec string_of_expr = function
-  IntLit(l) -> string_of_int l
-  | FloatLit(l) -> string_of_float l
-  | StringLit(l) -> l
-  | BoolLit(true) -> "true"
-  | BoolLit(false) -> "false"
-  | MatrixLit(el) -> "matrix lit"
-  | Id(s) -> s
-  | Binop(e1, o, e2) ->
-      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
-  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
-  | Call(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
-  | Noexpr -> ""
-
-let rec string_of_stmt = function
-    Block(stmts) ->
-      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
-  | Expr(expr) -> string_of_expr expr ^ ";\n";
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | For(e1, e2, e3, s) ->
-      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
-
-let rec string_of_typ = function
-    Int -> "int"
-  | Float -> "float"
-  | String -> "string"
-  | Bool -> "bool"
-  | Void -> "void"
-  | Matrix(t,d1,d2) -> "matrix<" ^ string_of_typ t ^ "> (" ^
-   string_of_int d1 ^ ", " ^ string_of_int d2 ^ ")"
-
-let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
-
-let string_of_fdecl fdecl =
-  string_of_typ fdecl.typ ^ " " ^
-  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
-  ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
-  String.concat "" (List.map string_of_stmt fdecl.body) ^
-  "}\n"
-
-let string_of_program (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
+(* Start Symbol *)
+type program = var_dec list * func_decl list
